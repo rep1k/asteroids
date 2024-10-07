f1b255416c428478eec6b8b6ebbb3ade70b86996
Repik <jakub.martinak@outlook.com>
2024-10-07 10:44:04 +0200

    Working draw function and rotation of player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-import constants
-import player as p
+from constants import *
+from player import Player
+
 
 
 def print_start_message():
@@ -9,23 +10,26 @@ def print_start_message():
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(
-        (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    time_object = pygame.time.Clock()
-    player = p.Player(
-        x=(constants.SCREEN_WIDTH / 2),
-        y=(constants.SCREEN_HEIGHT / 2),
-    )
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill("black")
-        pygame.display.flip()
         player.draw(screen)
-        dt = time_object.tick(60) / 1000
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+ 
