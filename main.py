bd6ac20d889a86f2e4b097040b4258c5b1c2fba6
Repik <jakub.martinak@outlook.com>
2024-10-07 10:39:59 +0200

    Added player class but drawing not working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player as p
 
 
 def print_start_message():
@@ -10,12 +11,20 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(
         (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    time_object = pygame.time.Clock()
+    player = p.Player(
+        x=(constants.SCREEN_WIDTH / 2),
+        y=(constants.SCREEN_HEIGHT / 2),
+    )
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        player.draw(screen)
+        dt = time_object.tick(60) / 1000
 
 
 if __name__ == "__main__":
